995a00d857cbbc2a9d5719070135093a38f99c61
TobDevTH <jirapat.kanwa@gmail.com>
2020-03-27 14:23:08 +0700

    Added Exercise4 file
    CP3 Exercise4 (Output Score)

---
+++ assignments/Exercise4_Jirapat_K.py
@@ -0,0 +1,10 @@
+fouEng = 82.4
+genBus = 93.7
+introCS = 90.8
+comP = 100.0
+
+print("---Your Score---")
+print("Foundation English : ", fouEng)
+print("General Business : ", genBus)
+print("Introduction to Computer Systems : ", introCS)
+print("Computer Programming : ", comP)
