ca2cbd8c7d3e0b0dc789bb10a08a5cc1b15aee33
TobDevTH <jirapat.kanwa@gmail.com>
2020-04-21 17:32:14 +0700

    Added Lecture50 file
    CP3 Lecture50 (Create function +, -, *, /)

---
+++ assignments/Lecture50_Jirapat_K.py
@@ -0,0 +1,14 @@
+x = int(input("First number : "))
+y = int(input("second number : "))
+def addNumber(x,y):
+    print("x + y =", x + y)
+def subtractNumber(x,y):
+    print("x - y =", x - y)
+def multiplyNumber(x,y):
+    print("x * y =", x * y)
+def divideNumber(x,y):
+    print("x / y =", x / y)
+addNumber(x,y)
+subtractNumber(x,y)
+multiplyNumber(x,y)
+divideNumber(x,y)
